df07dce76ead50aaa39c864f3c61e1e84b262280
Roman Frolov <f.roman34@mail.ru>
2023-10-19 02:43:13 +0300

    Попытка рефакторинга

--- src/category_processing.py
+++ src/category_processing.py
@@ -1,120 +1,98 @@
 import requests
 
+from configs import (
+    config,
+    secret_config,
+)
 
-def process_categories(
-        headers: dict,
-        categories_url: str,
-        existed_categories_url: str,
-        recipient_marketplace_id: int,
-        product_types_url: str,
-):
-    categories_response = requests.post(categories_url, headers=headers)
+
+def process_categories():
+    ozon_categories_url = config.get('ozon', 'categories_url')
+
+    client_id = secret_config.get('ozon', 'client_id')
+    api_key = secret_config.get('ozon', 'api_key')
+    headers = {'Client-Id': client_id, 'Api-Key': api_key}
+
+    ozon_response = requests.post(ozon_categories_url, headers=headers)
     
-    if categories_response.status_code != 200:
-        print(f'Unexpected response from the server:\n{categories_response.status_code}\n')
+    if ozon_response.status_code != 200:
+        print(f'Unexpected response from the server.\nCODE: {ozon_response.status_code}\n')
 
-    categories = categories_response.json().get('result')
-    existed_categories_response = requests.get(existed_categories_url)
+        return
 
-    if existed_categories_response.status_code != 200:
-        print(f'Unexpected response from the server:\n{existed_categories_response.status_code}\n')
+    ozon_categories = ozon_response.json().get('result')
 
-
-    existed_category_external_ids = set(
-        category.get('external_id') for category in existed_categories_response.json()
-    )
-
-    unpacked_product_types = unpack_categories(
-        categories,
-        existed_category_external_ids,
-        recipient_marketplace_id,
-        existed_categories_url,
-    )
-
-    send_product_types(unpacked_product_types.values(), product_types_url)
+    unpacked_categories = unpack_categories(ozon_categories)
 
 
-# TODO: Пофиксить присвоение нескольких категорий одному типу товара, разделить логику создания категории и создания типа товара
-def unpack_categories(
-        categories,
-        existed_ids,
-        marketplace_id,
-        existed_categories_url,
-        parent_id=None,
-) -> dict:
-    unpacked_product_types = {}
+def send_categories(ozon_categories, parent_id=None):
+    for ozon_category in ozon_categories:
+        category = dict(
+            external_id=ozon_category.get('category_id') or ozon_category.get('type_id'),
+            name=ozon_category.get('category_name') or ozon_category.get('type_name'),
+            recipient_marketplace=config.get('markets_bridge', 'marketplace_id'),
+            parent_categories=parent_id,
+        )
+        send_category(category)
 
-    for category_data in categories:
-
-        if product_type_id := category_data.get('type_id'):
-            if product_type_id in unpacked_product_types:
-                unpacked_product_types[product_type_id]['category'].append(parent_id)
-            else:
-                unpacked_product_types[product_type_id] = dict(
-                        external_id=product_type_id,
-                        name=category_data.get('type_name'),
-                        category=[parent_id],
-                )
-
-        else:
-            if category_data.get('category_id') not in existed_ids:
-                category = dict(
-                    external_id=category_data.get('category_id'),
-                    name=category_data.get('category_name'),
-                    recipient_marketplace=marketplace_id,
-                    parent=parent_id,
-                )
-                send_category(category, existed_categories_url)
-
-        if category_data['children']:
-            product_types = unpack_categories(
-                category_data['children'],
-                existed_ids,
-                marketplace_id,
-                existed_categories_url,
-                parent_id=category_data.get('category_id'),
-            )
-            unpacked_product_types.update(product_types)
-
-    return unpacked_product_types
+        if ozon_category['children']:
+            children = ozon_category.get('children')
+            parent_id = ozon_category.get('category_id')
+            send_categories(children, parent_id)
 
 
-def send_category(category, existed_categories_url):
-    post_response = requests.post(existed_categories_url, json=category)
+def send_category(category):
+    mb_categories_url = config.get('markets_bridge', 'recipient_categories_url')
+    mb_response = requests.post(mb_categories_url, json=category)
 
-    if post_response.status_code == 201:
-        category = post_response.json()
-        cat_id = category.get('id')
-        name = category.get('name')
-        post_message = f'Created: id = {cat_id}, name = {name}'
+    if mb_response.status_code == 201:
+        created_category = mb_response.json()
+        created_category_id = created_category.get('id')
+        created_category_name = created_category.get('name')
+        post_message = f'Created: id = {created_category_id}, name = {created_category_name}'
     else:
         post_message = (
             'Unexpected response from the server:\nCategory: '
-            f'{category.get('name')} ({post_response.status_code})\n'
+            f'{category.get('name')} (CODE: {mb_response.status_code})\n'
         )
 
     print(post_message)
 
 
-def send_product_types(product_types, product_types_url):
-    print('Creating product types...')
-
-    existed_product_types = requests.get(product_types_url).json()
-
-    existed_external_ids = set(record.get('external_id') for record in existed_product_types)
-
-    new_product_types = list(
-        filter(
-            lambda type: type.get('external_id') not in existed_external_ids, 
-            product_types
+def unpack_categories(ozon_categories: list, parent_id: int = None):
+    for ozon_category in ozon_categories:
+        category = dict(
+            external_id=ozon_category.get('category_id') or ozon_category.get('type_id'),
+            name=ozon_category.get('category_name') or ozon_category.get('type_name'),
+            recipient_marketplace=config.get('markets_bridge', 'marketplace_id'),
+            parent_categories=parent_id,
         )
-    )
-    
-    post_response = requests.post(product_types_url, json=new_product_types)
+        send_category(category)
 
-    if post_response.status_code == 201:
-        post_message = f'Created: {len(post_response.json())}\n'
-    else:
-        post_message = f'Unexpected response from the server:\n{post_response.status_code}\n'
+        if ozon_category['children']:
+            children = ozon_category.get('children')
+            parent_id = ozon_category.get('category_id')
+            send_categories(children, parent_id)
 
-    print(post_message)
+
+def unpack_categories(ozon_categories: list, parents: list = None):
+    result = []
+    if parents is None:
+        parents = []
+
+    for category in ozon_categories:
+        name = category.get('name')
+        external_id = category.get('category_id') or category.get('type_id'),
+        name = category.get('category_name') or category.get('type_name'),
+        recipient_marketplace = config.get('markets_bridge', 'marketplace_id'),
+        parent_categories = parent_id,
+        current_parents = parents + [name]
+
+        # Добавляем текущий элемент
+        result.append({'name': name, 'parents': current_parents})
+
+        # Рекурсивно обрабатываем вложенные элементы, если они есть
+        if 'children' in item:
+            result.extend(flatten_tree(item['children'], current_parents))
+
+    return result
--- src/main.py
+++ src/main.py
@@ -6,37 +6,11 @@ from category_processing import (
 from characteristic_processing import (
     process_characteristics,
 )
-from config import (
-    config,
-    secret_config,
-)
 
 
 def main():
-    client_id = secret_config.get('ozon', 'client_id')
-    api_key = secret_config.get('ozon', 'api_key')
-    headers = {'Client-Id': client_id, 'Api-Key': api_key}
-
-    ozon_categories_url = config.get('ozon', 'categories_url')
-    mb_categories_url = config.get('markets_bridge', 'recipient_categories_url')
-    marketplace_id = config.get('markets_bridge', 'marketplace_id')
-    process_categories(
-        headers,
-        ozon_categories_url,
-        mb_categories_url,
-        marketplace_id,
-    )
-
-    ozon_characteristics_url = config.get('ozon', 'category_attributes_url')
-    mb_characteristics_url = config.get('markets_bridge', 'recipient_characteristics_url')
-    related_types_url = config.get('markets_bridge', 'related_types_url')
-
-    process_characteristics(
-        headers,
-        ozon_characteristics_url,
-        mb_characteristics_url,
-        related_types_url,
-    )
+    process_categories()
+    process_characteristics()
 
 
 if __name__ == '__main__':
