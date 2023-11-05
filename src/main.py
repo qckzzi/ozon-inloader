639cfc7ec295bcac24e89a50d3da277140bf6f4d
Roman Frolov <f.roman34@mail.ru>
2023-11-05 20:47:42 +0300

    develop - Оптимизация сохранения значений в базу

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@
 """Основной модуль запуска загрузчика."""
 import argparse
 
+import requests
+
+import config
 from markets_bridge.services import (
     Formatter,
     Sender,
@@ -29,11 +32,15 @@ def main():
 
     characteristic_values = fetcher.get_characteristic_values()
     formatted_characteristic_values = Formatter.format_characteristic_values(characteristic_values)
-    Sender.send_characteristic_values(formatted_characteristic_values)
+    existed_characteristic_values = requests.get(config.mb_characteristic_values_url).json()
+    existed_value_ids = {value.get('external_id') for value in existed_characteristic_values}
+    not_existed_values = list(filter(lambda x: x.external_id not in existed_value_ids, formatted_characteristic_values))
+    Sender.send_characteristic_values(not_existed_values)
 
-    categories = fetcher.get_categories()
-    formatted_categories = Formatter.format_categories(categories)
-    Sender.send_categories(formatted_categories)
+    if not args.category_id:
+        categories = fetcher.get_categories()
+        formatted_categories = Formatter.format_categories(categories)
+        Sender.send_categories(formatted_categories)
 
 
 if __name__ == '__main__':
