beaace63eaa40461fb38ee985b34329149197eb6
Roman Frolov <f.roman34@mail.ru>
2023-11-15 12:24:05 +0300

    develop - Добавлена аутентификация в Markets-Bridge; - Изменен принцип работы с Клиент-серверной архитектуры на producer-consumer

--- src/main.py
+++ src/main.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python
+import json
 import logging
 
+import pika
 import requests
-from fastapi import (
-    FastAPI,
-)
 
 import config
-from markets_bridge.services import (
+from markets_bridge.utils import (
     Formatter,
     Sender,
+    get_existed_characteristic_values,
+    write_log_entry,
 )
-from ozon.services import (
+from ozon.utils import (
     Fetcher,
 )
 
 
-app = FastAPI()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+def callback(ch, method, properties, body):
+    try:
+        message = json.loads(body)
+        operation_type = message['method']
 
+        logging.info(f'Message was received. Operation is "{operation_type.lower()}"')
 
-# TODO: Вынести бизнес-логику из ручки и декомпозировать ее
-@app.get('/load_ozon_attributes_for_category/')
-def load_ozon_attributes_for_category(category_id: int):
+        match operation_type:
+            case 'LOAD_FOR_CATEGORY':
+                load_ozon_attributes_for_category(message['category_external_id'], message['matching_id'])
+            case 'LOAD_CATEGORIES':
+                load_categories()
+
+    except KeyError as e:
+        error = f'Body validation error: {e}'
+        write_log_entry(error)
+        logging.error(error)
+        return
+    except Exception as e:
+        error = f'There was a problem: {e}'
+        write_log_entry(error)
+        logging.exception(error)
+        return
+
+def load_ozon_attributes_for_category(category_id: int, matching_id: int):
     fetcher = Fetcher()
 
     characteristics = fetcher.get_characteristics(category_id)
@@ -32,13 +51,14 @@ def load_ozon_attributes_for_category(category_id: int):
     characteristic_values = fetcher.get_characteristic_values()
     formatted_characteristic_values = Formatter.format_characteristic_values(characteristic_values)
 
-    existed_characteristic_values = requests.get(config.mb_characteristic_values_url).json()
+    existed_characteristic_values = get_existed_characteristic_values()
     existed_value_ids = {value.get('external_id') for value in existed_characteristic_values}
     not_existed_values = list(filter(lambda x: x.external_id not in existed_value_ids, formatted_characteristic_values))
 
     Sender.send_characteristic_values(not_existed_values)
+    body = {'category_matching_id': matching_id}
+    requests.post(config.mb_create_characteristic_matchings_url, json=body)
 
-@app.get('/load_categories/')
 def load_categories():
     fetcher = Fetcher()
 
@@ -50,3 +70,14 @@ def load_categories():
     not_existed_categories = list(filter(lambda x: x.external_id not in existed_value_ids, formatted_categories))
 
     Sender.send_categories(not_existed_categories)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+
+    connection_parameters = pika.ConnectionParameters(host='localhost', heartbeat=300, blocked_connection_timeout=300)
+    with pika.BlockingConnection(connection_parameters) as connection:
+        channel = connection.channel()
+        channel.queue_declare('inloading')
+        channel.basic_consume('inloading', callback, auto_ack=True)
+        channel.start_consuming()
--- src/markets_bridge/utils.py
+++ src/markets_bridge/utils.py
@@ -139,14 +139,144 @@ class Sender:
         for obj in objects:
             cls._send_object(obj, **kwargs)
 
-    @staticmethod
-    def _send_object(obj, url, name, display_field):
-        response = requests.post(url, json=vars(obj))
+    @classmethod
+    def _send_object(cls, obj, url, name, display_field):
+        headers = get_authorization_headers()
+        response = requests.post(url, json=vars(obj), headers=headers)
+
+        if response.status_code == 401:
+            accesser = Accesser()
+            accesser.update_access_token()
+            cls._send_object(obj, url, name, display_field)
+
+            return
+
         display_value = getattr(obj, display_field)
 
         if response.status_code == 201:
             logging.info(f'The "{display_value}" {name} has been created.')
         elif response.status_code == 200:
             logging.info(f'The "{display_value}" {name} already exists.')
-        else:
-            logging.error(f'When creating the "{display_value}" {name}, the server returned an error.')
+
+
+class Singleton:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
+
+class Accesser(Singleton):
+    """Получатель доступа к сервису Markets-Bridge.
+
+    При первичном получении токена доступа генерируется JWT. При истечении access токена необходимо вызывать
+    update_access_token(). В случае, если refresh токен умер, вызывается метод update_jwt().
+    """
+
+    def __init__(self):
+        if not self._initialized:
+            self._refresh_token = None
+            self._access_token = None
+
+            self._initialized = True
+
+    @property
+    def access_token(self) -> str:
+        if not self._access_token:
+            self.update_jwt()
+
+        return self._access_token
+
+    def update_jwt(self):
+        login_data = {
+            'username': config.mb_login,
+            'password': config.mb_password
+        }
+
+        response = requests.post(config.mb_token_url, data=login_data)
+        response.raise_for_status()
+        token_data = response.json()
+        self._access_token = token_data['access']
+        self._refresh_token = token_data['refresh']
+
+    def update_access_token(self):
+        body = {'refresh': self._refresh_token}
+
+        response = requests.post(config.mb_token_refresh_url, json=body)
+
+        if response.status_code == 401:
+            self.update_jwt()
+            response = requests.post(config.mb_token_refresh_url, json=body)
+
+        response.raise_for_status()
+
+        token_data = response.json()
+        self._access_token = token_data['access']
+
+
+def write_log_entry(message: str):
+    """Создает записи логов в сервисе Markets-Bridge."""
+
+    body = {'service_name': 'OZON attributes inloader', 'entry': message}
+    headers = get_authorization_headers()
+    response = requests.post(config.mb_logs_url, json=body, headers=headers)
+
+    if response.status_code == 401:
+        accesser = Accesser()
+        accesser.update_access_token()
+
+        return write_log_entry(message)
+
+    response.raise_for_status()
+
+
+def get_ozon_client_id() -> str:
+    return get_system_environment('OZON_CLIENT_ID')
+
+
+def get_ozon_api_key() -> str:
+    return get_system_environment('OZON_API_KEY')
+
+
+def get_system_environment(environment_key: str) -> str:
+    """Возвращает системную переменную из сервиса Markets-Bridge по ключу."""
+
+    headers = get_authorization_headers()
+    response = requests.get(f'{config.mb_system_environments_url}{environment_key}/', headers=headers)
+
+    if response.status_code == 401:
+        accesser = Accesser()
+        accesser.update_access_token()
+
+        return get_system_environment(environment_key)
+
+    response.raise_for_status()
+    result = response.json()['value']
+
+    return result
+
+
+def get_existed_characteristic_values() -> dict:
+    headers = get_authorization_headers()
+    response = requests.get(config.mb_characteristic_values_url, headers=headers)
+
+    if response.status_code == 401:
+        accesser = Accesser()
+        accesser.update_access_token()
+
+        return get_existed_characteristic_values()
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_authorization_headers() -> dict:
+    accesser = Accesser()
+    access_token = accesser.access_token
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    return headers
